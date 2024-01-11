de75a4749fc40d9eba6f989c6325d14243077fc5
Michaelmira <151809512+Michaelmira@users.noreply.github.com>
2024-01-11 01:59:17 +0000

    Previous project excuse generator skeleton installed, replaced excuse syntax with Domain generator theme and added 8 domains to be cycled though

--- src/app.js
+++ src/app.js
@@ -7,5 +7,55 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-Domain").innerHTML = generateDomain();
+    document.querySelector("#the-Domain2").innerHTML = generateDomain();
+    document.querySelector("#the-Domain3").innerHTML = generateDomain();
+    document.querySelector("#the-Domain4").innerHTML = generateDomain();
+    document.querySelector("#the-Domain5").innerHTML = generateDomain();
+    document.querySelector("#the-Domain6").innerHTML = generateDomain();
+    document.querySelector("#the-Domain7").innerHTML = generateDomain();
+    document.querySelector("#the-Domain8").innerHTML = generateDomain();
+  });
+};
+let generateDomain = () => {
+  let whatWhereWhen = ["A", "The", "Some", "El"];
+  let adjective = [
+    "coolest",
+    "best",
+    "awesome",
+    "premier",
+    "craziest",
+    "better",
+    "astonishing",
+    "majestic",
+    "wonderful"
+  ];
+  let place = [
+    "place",
+    "palace",
+    "club",
+    "space",
+    "domain",
+    "mansion",
+    "chateau",
+    "manor",
+    "castle"
+  ];
+  let domain = [".org", ".com", ".sexy", ".space", ".net"];
+
+  let whatWhereWhenIndex = Math.floor(Math.random() * whatWhereWhen.length);
+  let adjectiveIndex = Math.floor(Math.random() * adjective.length);
+  let placeIndex = Math.floor(Math.random() * place.length);
+  let domainIndex = Math.floor(Math.random() * domain.length);
+
+  return (
+    whatWhereWhen[whatWhereWhenIndex] +
+    "" +
+    adjective[adjectiveIndex] +
+    "" +
+    place[placeIndex] +
+    "" +
+    domain[domainIndex]
+  );
 };
