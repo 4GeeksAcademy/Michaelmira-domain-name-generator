ef4bc14164a1df1e61eb799bc7be1d19c24f2df2
Michaelmira <151809512+Michaelmira@users.noreply.github.com>
2024-01-13 19:58:49 +0000

    Finished Domain name generator

--- src/app.js
+++ src/app.js
@@ -5,35 +5,49 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-// window.onload = function() {
-// This comment is to prevent the autofromatter for doing stuff for a second.
-let whatWhereWhen = ["A", "The", "Some", "El", "An", "any"];
-let adjective = [
-  "coolest",
-  "best",
-  "awesome",
-  "premier",
-  "craziest",
-  "better",
-  "astonishing",
-  "majestic",
-  "wonderful"
-];
-let place = [
-  "place",
-  "palace",
-  "club",
-  "space",
-  "domain",
-  "mansion",
-  "chateau",
-  "manor",
-  "castle"
-];
-let domain = [".org", ".com", ".sexy", ".space", ".net", "cat", "dog"];
+window.onload = function() {
+  // This comment is to prevent the autofromatter for doing stuff for a second.
+  let whatWhereWhens = ["A", "The", "Some", "El", "An", "any"];
+  let adjectives = [
+    "coolest",
+    "best",
+    "awesome",
+    "premier",
+    "craziest",
+    "better",
+    "astonishing",
+    "majestic",
+    "wonderful"
+  ];
+  let places = [
+    "place",
+    "palace",
+    "club",
+    "space",
+    "domain",
+    "mansion",
+    "chateau",
+    "manor",
+    "castle"
+  ];
+  let domains = [".org", ".com", ".sexy", ".space", ".net", "cat", "dog"];
 
-console.log(place);
-// };
+  let domainNames = [];
+
+  for (let whatWhereWhen of whatWhereWhens) {
+    for (let adjective of adjectives) {
+      for (let place of places) {
+        for (let domain of domains) {
+          domainNames.push(`${whatWhereWhen}${adjective}${place}${domain}`);
+        }
+      }
+    }
+  }
+
+  document.body.innerHTML = `<ul>${domainNames
+    .map(domainName => `<li>${domainName}</li>`)
+    .join(" ")}</ul>`;
+};
 
 // window.onload = function() {
 //   //write your code here
